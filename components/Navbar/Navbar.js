cf437f8c4d06f355452e2c0890182e48dbd7e610
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-18 23:52:01 +0530

    added navbar  component with their css

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+
+function Navbar(props) {
+  const [signOut, setSignOut] = useState(false);
+  const { username } = props;
+  const router = useRouter();
+
+  const handleHomeClick = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleListClick = () => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Link href="/" legacyBehavior>
+          <a className={styles.logoLink}>
+            <div className={styles.logoWrapper}>
+              <Image
+                src="/static/netflix.svg"
+                alt="Netflix Logo"
+                width={128}
+                height={34}
+              />
+            </div>
+          </a>
+        </Link>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleHomeClick}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleListClick}>
+            My List
+          </li>
+        </ul>
+        <nav className={styles.navCotainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p
+                className={styles.username}
+                onClick={() => setSignOut(!signOut)}
+              >
+                {username}
+              </p>
+              {/* expand more icons */}
+              <Image
+                src="/static/expand_more.svg"
+                alt="Expand"
+                width={24}
+                height={24}
+                style={{ color: "White" }}
+              />
+            </button>
+            {signOut && (
+              <div className={styles.navDropdown}>
+                <div >
+                  <Link href="/login" legacyBehavior >
+                    <a className={styles.linkName}>Sign Out</a>
+                  </Link>
+                  <div className={styles.lineWrapper}></div>
+                </div>
+              </div>
+            )}
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
