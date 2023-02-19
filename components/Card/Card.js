13957d082908dba1303c99811bbf055007004e9e
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-19 18:39:47 +0530

    modified

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,10 +1,13 @@
 import styles from "./Card.module.css";
 import Image from "next/image";
 import { useState } from "react";
+import { motion } from "framer-motion";
+import cls from "classnames";
 
 function Card({
   imgUrl = "https://images.unsplash.com/photo-1594908900066-3f47337549d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
   size = "medium",
+  id
 }) {
   const itemMap = {
     large: styles.lgItem,
@@ -19,10 +22,14 @@ function Card({
     );
   };
 
+  const scale = (id === 0) ? { scaleY: 1.1 } : { scale: 1.1 };
+
   return (
-    <div>
-      Card
-      <div className={itemMap[size]}>
+    <div className={styles.container}>
+      <motion.div
+        className={cls(styles.imgMotionWrapper, itemMap[size])}
+        whileHover={{ ...scale }}
+      >
         <Image
           src={imgSrc}
           alt="picture"
@@ -30,7 +37,7 @@ function Card({
           className={styles.cardImg}
           onError={handleOnError}
         />
-      </div>
+      </motion.div>
     </div>
   );
 }
---
+++ components/Card/SectionCards.js
@@ -0,0 +1,15 @@
+import Card from "./Card";
+import styles from "./SectionCard.module.css";
+
+function SectionCards({ title, videos, size }) {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.cardWrapper}>
+        {videos.map((video, idx) => <Card id={idx} imgUrl={video.imgUrl} size={size} />)}
+      </div>
+    </section>
+  );
+}
+
+export default SectionCards;
