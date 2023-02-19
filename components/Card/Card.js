6461b3cea5318aadf31736d7187b118890369e82
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-02-19 16:11:56 +0530

    added card component with their css

---
+++ components/Card/Card.js
@@ -0,0 +1,38 @@
+import styles from "./Card.module.css";
+import Image from "next/image";
+import { useState } from "react";
+
+function Card({
+  imgUrl = "https://images.unsplash.com/photo-1594908900066-3f47337549d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  size = "medium",
+}) {
+  const itemMap = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+  const [imgSrc, setImgSrc] = useState(imgUrl);
+
+  const handleOnError = () => {
+    setImgSrc(
+      "https://images.unsplash.com/photo-1594908900066-3f47337549d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    );
+  };
+
+  return (
+    <div>
+      Card
+      <div className={itemMap[size]}>
+        <Image
+          src={imgSrc}
+          alt="picture"
+          layout="fill"
+          className={styles.cardImg}
+          onError={handleOnError}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
