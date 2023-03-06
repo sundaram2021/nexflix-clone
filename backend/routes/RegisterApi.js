a1bfd11f8e58a1ed6b1a8fb89131b7820f9e07b4
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-06 18:45:07 +0530

    added login and register routes

---
+++ backend/routes/RegisterApi.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { register, login } from "../controllers/RegisterApiController.js";
+
+const router = Router();
+
+router.post("/register", register)
+router.post("/login", login)
+// router.get('/login', login)
+
+export default router;
