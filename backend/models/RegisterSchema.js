fc9d81be673fe54c179f9518d21c5ce185da5cd5
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-06 23:03:49 +0530

    modified

--- backend/models/RegisterSchema.js
+++ backend/models/RegisterSchema.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const { Schema } = mongoose
 
 const RegistrationSchema = new Schema({
-    firstName: String,
-    lastName: String,
     email: String,
     password: String,
 })
