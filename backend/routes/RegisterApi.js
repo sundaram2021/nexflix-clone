83019b7e6ceb2eafe026f1584f6459e97ceff7b8
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-19 19:58:37 +0530

    added register route

--- backend/routes/RegisterApi.js
+++ backend/routes/RegisterApi.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register, login, getEmail } from "../controllers/RegisterApiController.js";
+import { register, login, getEmail, HomeGet } from "../controllers/RegisterApiController.js";
 
 const router = Router();
 
 router.post("/register", register)
 router.post("/login", login)
 router.get('/getemail', getEmail)
+router.get("/", HomeGet)
 
 export default router;
