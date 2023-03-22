caf5712e6ee9da0bad684659f998bbddb9821db6
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-03-22 22:58:40 +0530

    modified and ajusted according to api

--- components/lib/videos.js
+++ components/lib/videos.js
@@ -1,26 +1,44 @@
-import videoData from "../Data/videos.json";
+import videoTestData from "../Data/videos.json";
+
+
+const fetchVideos = async(url) =>  {
+  const API_KEY = process.env.API_KEY;
+  const BASE_URL = 'youtube.googleapis.com/youtube/v3';
+
+  const res = await fetch(
+    `https://${BASE_URL}/${url}&maxResults=25&key=${API_KEY}`
+  );
+  return await res.json();
+} 
+
 
 export const getCommonVideos = async (url) => {
-  const API_KEY = process.env.API_KEY;
+  
   try {
-    const BASE_URL = 'youtube.googleapis.com/youtube/v3';
+    
+    const isDev = process.env.DEVELOPMENT;
+    const data = isDev ? videoTestData : await fetchVideos(url);
 
-    const res = await fetch(
-      `https://${BASE_URL}/${url}&maxResults=25&key=${API_KEY}`
-    );
-    const data = await res.json();
     if (data?.error) {
       console.error("Youtube API error", data.error);
       return [];
     }
 
+    
+
     return data?.items.map((item) => {
       const id = item.id?.videoId || item.id;
-
+    
+      // console.log(item);
+      const snippet = item.snippet;
       return {
-        title: item.snippet.title,
-        imgUrl: item.snippet.thumbnails.high.url,
+        title: snippet.title,
+        imgUrl: snippet.thumbnails.high.url,
         id,
+        description: snippet.description,
+        publishTime: snippet.publishedAt,
+        channelTitle: snippet.channelTitle,
+        statistics: item.statistics ? item.statistics : { viewCount: 0 },
       };
     });
   } catch (err) {
@@ -38,5 +56,12 @@ export const getVideos = (searchQuery) => {
 export const getPopularVideos = () => {
   const URL =
     "videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN";
+  return getCommonVideos(URL);  
+}
+
+
+export const getYoutubeVideoById = (videoId) => {
+  const URL =
+  `videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}`;
   return getCommonVideos(URL);  
 }
